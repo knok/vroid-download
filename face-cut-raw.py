342168fdc2165b00ecbea1f6e00ac55b5319c28b
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2019-02-22 14:34:31 +0900

    crop by detection rect

---
+++ face-cut-raw.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+
+import glob
+import os
+
+import cv2
+
+jpg_pat = 'seiga-thumb/*.jpg'
+files = glob.glob(jpg_pat)
+cascade = cv2.CascadeClassifier("lbpcascade_animeface.xml")
+
+def detect_save(fname, outbase):
+    image = cv2.imread(fname, cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+    
+    faces = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)# , minsize=(24, 24))
+    for i, (x, y, w, h) in enumerate(faces):
+        print(x, y, w, h)
+        ofname = "%s_%d.png" % (outbase, i)
+        rimg = image[y:y+h, x:x+w]
+        cv2.imwrite(ofname, rimg)
+    return
+
+# 逐次処理して結果を出力
+outdir = 'faces'
+for fname in files[0:1]:
+    imgidm, _ = os.path.splitext(os.path.basename(fname))
+    basename = os.path.basename(fname)
+    outfname = os.path.join(outdir, basename)
+    detect_save(fname, outfname)

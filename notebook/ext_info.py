67b21f9bcb5a119b89b113567eca55b8fdaf577f
NOKUBI Takatsugu <knok@daionet.gr.jp>
2022-03-22 15:12:17 +0900

    output url, password list

--- notebook/ext_info.py
+++ notebook/ext_info.py
@@ -18,6 +18,8 @@ logging.basicConfig(level=logging.INFO)
 
 db = dbm.open('seiga-info.db', 'c')
 urlpat = re.compile('https?://[\w!?/+\-_~=;.,*&@#$%()\'[\]]+')
+pass_pat = re.compile('(?:PASS|パス)(?:ワード|word)?[:：= ]*(?:[\sは＝⇒・ ])*[\[(「【]?([^]」】\n)　]*)[\]())で」】]?', re.MULTILINE | re.IGNORECASE)
+pass_pat_nl = re.compile('(?:PASS|パス)(?:ワード|word)?.*\n\s*[\[(「【]?([^]」】\n]*)[\])」】]?', re.MULTILINE| re.IGNORECASE)
 
 def get_args() -> argparse.Namespace:
     p = argparse.ArgumentParser()
@@ -42,21 +44,40 @@ def ext_description(fname):
         return ''
     return divs[0].text
 
+def get_password(text):
+    # if 'パス' in text or 'pass' in text.lower():
+    #     logger.info(f'contains pass: {text}')
+    match = pass_pat.search(text)
+    if not match is None:
+        cand = match.group(1)
+        if cand != '':
+            return cand # 発見パスワード
+    match = pass_pat_nl.search(text)
+    if match is None:
+        return ''
+    return match.group(1)
+
 def ext_info(text):
+    passwd =  get_password(text)
     match = urlpat.findall(text)
-    return match
+    if len(match) == 0:
+        match = ''
+    else:
+        match = match[0]
+    return match, passwd
 
 def main():
     args = get_args()
     files = get_files(args.file_pat)
     for file in tqdm.tqdm(files):
         # logger.info(file)
-        url = fname2url(file)
+        origurl = fname2url(file)
         text = ext_description(file)
         # print(text)
-        match = ext_info(text)
-        print(match)
-        logger.info(url)
+        url, passwd = ext_info(text)
+        # print(match)
+        # logger.info(f'URL:{url}, pass:{passwd}')
+        print(f'page:{origurl}, download:{url}, pass:{passwd}')
 
 if __name__ == '__main__':
     main()

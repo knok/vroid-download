865c968311136ead5b63e4f42dcc0d773240dec1
NOKUBI Takatsugu <knok@daionet.gr.jp>
2022-03-24 17:22:40 +0900

    refactoring: extract method

--- scripts/batch-load.py
+++ scripts/batch-load.py
@@ -19,11 +19,13 @@ p = argparse.ArgumentParser()
 p.add_argument('--input', default='dl-list.csv')
 p.add_argument('--out', default='./downloads')
 p.add_argument('--log', default='logfile.txt')
+p.add_argument('--type', default='axfc', type=str)
 args = p.parse_args()
 
 cmd = ['python3']
 
 DOMAIN_GETUP = 'getuploader.com'
+DOMAIN_AXFC = 'axfc.net'
 
 # getupload
 cmd += ['scripts/dl-getup.py', '-n']
@@ -46,30 +48,33 @@ def has_file(outdir):
         return False
     return True
 
+def handle_getuploader(iid, url, passwd) -> bool:
+    # 過去のエラーチェック
+    if has_error(args.log, url):
+        logger.info(f'url {url} had an fetch error at the last try, skipping')
+        return False
+    # ファイルチェック
+    outdir = f'{args.out}/{iid}'
+    if has_file(outdir):
+        logger.warning(f'dir {outdir} has already downloaded file, skipping')
+        return False
+    callcmd = cmd.copy()
+    callcmd.append(url)
+    callcmd.append('-o')
+    callcmd.append(outdir)
+    if passwd != '':
+        callcmd.append('-p')
+        callcmd.append(passwd.strip())
+    logger.info(f'call cmd: {callcmd}')
+    ret = subprocess.check_call(callcmd)
+    if not has_file(outdir):
+        logging_error_url(args.log, url)
+    return True
+
 with open(args.input, 'r') as f:
     reader = csv.reader(f)
     next(reader)
     for row in reader:
         iid, _, url, passwd = row
         if DOMAIN_GETUP in url:
-            # 過去のエラーチェック
-            if has_error(args.log, url):
-                logger.info(f'url {url} had an fetch error at the last try, skipping')
-                continue
-            # ファイルチェック
-            outdir = f'{args.out}/{iid}'
-            if has_file(outdir):
-                logger.warning(f'dir {outdir} has already downloaded file, skipping')
-                continue
-            callcmd = cmd.copy()
-            callcmd.append(url)
-            callcmd.append('-o')
-            callcmd.append(outdir)
-            if passwd != '':
-                callcmd.append('-p')
-                callcmd.append(passwd.strip())
-            logger.info(f'call cmd: {callcmd}')
-            ret = subprocess.check_call(callcmd)
-            if not has_file(outdir):
-                logging_error_url(args.log, url)
-
+            handle_getuploader(iid, url, passwd)

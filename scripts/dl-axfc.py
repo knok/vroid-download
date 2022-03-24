3fb63fc9625cb2c94da5c07a89314c248b39e383
NOKUBI Takatsugu <knok@daionet.gr.jp>
2022-03-24 17:47:55 +0900

    download from axfc

---
+++ scripts/dl-axfc.py
@@ -0,0 +1,95 @@
+#
+"""
+axfc.net からダウンロードを行う
+"""
+
+import os
+import sys
+import argparse
+import time
+import logging
+import subprocess
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchElementException
+
+p = argparse.ArgumentParser()
+p.add_argument('url')
+p.add_argument('--password', '-p', default='')
+p.add_argument('--output-dir', '-o', default='out')
+p.add_argument('--no-headless', '-n', default=False, action='store_true')
+args = p.parse_args()
+
+dldir = os.path.abspath(args.output_dir)
+# ref: https://www.browserstack.com/guide/download-file-using-selenium-python
+profile = webdriver.FirefoxProfile()
+profile.set_preference("browser.download.folderList", 2)
+profile.set_preference("browser.download.manager.showWhenStarting", False)
+profile.set_preference("browser.download.dir", dldir)
+#Example:profile.set_preference("browser.download.dir", "C:\Tutorial\down")
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
+
+options = Options()
+if not args.no_headless:
+  options.add_argument('--headless')
+
+driver:WebDriver = webdriver.Firefox(firefox_profile=profile, options=options)
+
+url = args.url
+if url.find('?key=') < 0:
+    # keyをもっていない場合
+    if args.password != '':
+        url += f'?key='
+    else:
+        # パスワード情報なし
+        pass
+
+driver.get(args.url)
+
+# キーワードチェック
+try:
+    keyword = driver.find_element_by_css_selector('input[name="keyword"]')
+except NoSuchElementException as e:
+    logger.error(f'no keyword input element: {e}')
+    driver.close()
+    sys.exit(0)
+
+if keyword.text == '':
+    keyword.send_keys(args.password)
+
+# ボタンのチェック
+try:
+    button = driver.find_element_by_css_selector('input.button')
+except NoSuchElementException as e:
+        logger.error(f'no input button: {e}')
+        driver.close()
+        sys.exit(0)
+
+# ダウンロードページ遷移
+button.click()
+
+anchors = driver.find_elements_by_css_selector('a')
+for target in anchors:
+    if target.text.find('ダウンロードする') > 0:
+        break
+# ダウンロード開始
+target.click()
+
+anchors = driver.find_elements_by_css_selector('a')
+for target in anchors:
+    if target.text.find('こちら') >= 0:
+        break
+# 強制ダウンロード
+# target.click()
+link = target.get_attribute('href')
+os.chdir(dldir)
+subprocess.call(['wget', link])
+
+time.sleep(5)
+
+driver.close()
